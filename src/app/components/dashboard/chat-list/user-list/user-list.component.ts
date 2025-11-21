6dd09e247d8c0b27592d15cf10fa01f1a667a794
ChandruMurugan-01 <chandrum690@gmail.com>
2025-11-21 09:57:40 +0530

    TVM-1004: Dashboard UI Structure

--- src/app/components/dashboard/chat-list/user-list/user-list.component.ts
+++ src/app/components/dashboard/chat-list/user-list/user-list.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent {
+users = [
+  { name: "Joshva", img: "assets/profile2.jpg", lastMessage: "tvm-chat.zip", time: "16:33" },
+  { name: "Priyatharshan", img: "assets/profile3.jpg", lastMessage: "Okk daa", time: "10:07" },
+  { name: "VTM Group", img: "assets/group.png", lastMessage: "Ok sir", time: "09:54" },
+  
+];
 
 }
